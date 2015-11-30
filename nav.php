6139545a8a6d790802398bf3b14603fa428f768e
Patrick Conley <patrick.bj.conley@gmail.com>
2015-11-29 17:36:27 -0800

    Fix navbar alignment

--- nav.php
+++ nav.php
@@ -1,8 +1,11 @@
-<a href="./index.php"><span>About the club</span></a>
-<a href="./festival/"><span>Festival</span></a>
-<a href="./photos.php"><span>Photos</span></a>
-<a href="./events.php"><span>Events</span></a>
-<a href="./contact.php"><span>Contact</span></a>
+<ul>
+   <li><a href="./index.php"><span>About the club</span></a></li>
+   <li><a href="./festival/"><span>Festival</span></a></li>
+   <li><a href="./photos.php"><span>Photos</span></a></li>
+   <li><a href="./events.php"><span>Events</span></a></li>
+   <li><a href="./contact.php"><span>Contact</span></a></li>
+</ul>
+
 <a href="https://www.facebook.com/groups/53251997026/">
       <img width=24 src="images/f_logo.png" title="Facebook link">
 </a>
